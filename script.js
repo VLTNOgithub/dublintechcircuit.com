785ea891a63a0536128a6b0e7804d76207f6e432
VLTNOgithub <gfree6311@gmail.com>
2025-08-08 16:35:35 +0100

    add collage (and its images), also various sponsor logos and other images

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.onload = () => {
+    var collage = document.querySelector(".collage");
 
+    // First, watch the collage for all images to load
+    imagesLoaded( collage, function() {
+        console.log("All images loaded, now running Masonry!");
+
+        var msnry = new Masonry(collage, {
+            itemSelector: ".collage-item",
+            percentPosition: true,
+            columnWidth: ".collage-sizer"
+        });
+
+        // Now we can safely add our random rotation
+        const items = document.querySelectorAll(".collage-item");
+        items.forEach(item => {
+            const rotation = (Math.random() * 10) - 5;
+            item.style.transform = `rotate(${rotation}deg)`;
+
+            item.style.zIndex = Math.floor(Math.random() * 5);
+        });
+    });
 }
